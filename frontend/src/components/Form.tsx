02d7609e296c0b57980a78d79f103e88068ec62c
Geneivie Nguyen <geneivienguyen@yahoo.com>
2024-10-04 00:14:55 -0500

    removed dotenv

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const Form: React.FC = () => {
   const [file, setFile] = useState<File | null>(null);
