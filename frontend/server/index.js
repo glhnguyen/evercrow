b1b4c49b4dc5c635831033f5f25f5a7cf5ba46d3
Geneivie Nguyen <geneivienguyen@yahoo.com>
2024-10-03 20:15:11 -0500

    created node server

---
+++ frontend/server/index.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+
+const port = process.env.SERVER_PORT;
+const environment = process.env.NODE_ENV;
+
+const buildPath = path.join(__dirname, "..", "build");
+const indexPath = path.join(buildPath, "index.html");
+
+const app = express();
+
+app.use(
+  express.urlencoded({ extended: true }),
+  express.json(),
+  express.static(buildPath)
+);
+
+app.use((req, res, next) => {
+  res.sendFile(indexPath);
+});
+
+app.listen(port, () => {
+  console.log(`Node Server listening on port: ${port} [${environment}]`);
+});
